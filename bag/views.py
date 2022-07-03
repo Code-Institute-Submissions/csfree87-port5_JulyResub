5f48d1d4df66b646a05d3ebb982a319f351a9fc9
csfree87 <csfree@icloud.com>
2022-07-03 20:12:05 +0000

    Added adjust and remove bag and calc subtotal templates

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -21,3 +21,32 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """ Adjust quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+    
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop[item_id]
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """ remove item from the shopping bag """
+
+    bag = request.session.get('bag', {})
+
+    del bag[item_id]
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
