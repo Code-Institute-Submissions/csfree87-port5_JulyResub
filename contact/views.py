0aaab3e1cc4384099f878eac118c6e1b10245ff9
csfree87 <csfree@icloud.com>
2022-07-07 01:43:26 +0000

    Added message sent to contact view

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact_view(request):
     form = ContactForm()
     context = {'form': form}
+    messages.success(request, ('Message Sent!'))
     return render(request, 'contact/contact.html', context)
